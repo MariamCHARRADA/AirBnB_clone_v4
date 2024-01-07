ab02ccc964736c9c096d1f7336443637da7a5f24
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-07 02:23:55 +0100

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,19 @@ from flask import jsonify
 def get_status():
     """returns status ok"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"])
+def get_stats():
+    """returns number of each objects"""
+    from models import storage
+
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(stats)
