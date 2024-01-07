446c62142163d645e36a9c298f12b1e740cb25cc
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-07 01:29:19 +0100

    json dumps

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """ Index module """
 from api.v1.views import app_views
-from flask import jsonify
+from flask import make_response, json
 
 
 @app_views.route("/status", methods=["GET"])
 def get_status():
     """returns status ok"""
-    return jsonify({"status": "OK"})
+    response_data = {"status": "OK"}
+    response_json = json.dumps(response_data)
+
+    response = make_response(response_json)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
