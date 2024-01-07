874d9906c38dad683b4433f554fbe00f292e2a91
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-08 00:43:03 +0100

    cities

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,7 +8,7 @@ from models import storage
 from flask import jsonify, abort, request
 
 
-@app_views.route("/places/<city_id>/places", methods=["GET"],
+@app_views.route("/cities/<city_id>/places", methods=["GET"],
                  strict_slashes=False)
 def get_places(city_id):
     """GET all places"""
