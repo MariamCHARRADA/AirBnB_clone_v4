b2b1e92162772f7b256f0764dd6265c29ba96dce
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-07 01:40:59 +0100

    shebang init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""init.py"""
 from flask import Blueprint
 
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 """ Index module """
 from api.v1.views import app_views
-from flask import make_response, json
+from flask import jsonify
 
 
 @app_views.route("/status", methods=["GET"])
 def get_status():
     """returns status ok"""
-    response_data = {"status": "OK"}
-    response_json = json.dumps(response_data)
-
-    response = make_response(response_json)
-    response.headers["Content-Type"] = "application/json"
-
-    return response
+    return jsonify({"status": "OK"})
