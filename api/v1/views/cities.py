f7203986238813d4aabdbab498566ed0c6332f61
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-08 00:16:00 +0100

    fix format

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -37,7 +37,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route("/states/<state_id>/cities", methods=["POST"], strict_slashes=False)
+@app_views.route("/states/<state_id>/cities", methods=["POST"],
+                 strict_slashes=False)
 def post_city(state_id):
     """POST one city"""
     if not request.get_json():
